0c74e380bba5989e304cb9b6326a1aaa63a8a40f
Garment-Lee <184813034@qq.com>
2025-09-07 00:23:09 +0800

    1、LeetCode面试经典150题之238-除自身以外数组的乘积；

--- src/com/ligf/algorithm/leetcode/interview/Topic238.java
+++ src/com/ligf/algorithm/leetcode/interview/Topic238.java
@@ -17,12 +17,12 @@ public class Topic238 {
         int[] right = new int[nums.length];
         int[] result = new int[nums.length];
         left[0] = 1;
-        right[nums.length - 1] = 0;
+        right[nums.length - 1] = 1;
         for (int i = 1; i < nums.length; i++){
             left[i] = nums[i - 1] * left[i - 1];
         }
-        for (int j = nums.length - 2; j >=0; j ++){
-            right[j] = right[j + 1];
+        for (int j = nums.length - 2; j >=0; j --){
+            right[j] = right[j + 1] * nums[j + 1];
         }
         for (int k = 0; k < nums.length; k++){
             result[k] = left[k] * right[k];
